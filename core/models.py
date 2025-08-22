d3cb8bc3371a4fdffc8e4e9d1a4f541ef0bdc281
DeWeWO <dewel000per@gmail.com>
2025-08-22 13:16:36 +0500

    masofaviy talim

--- core/models.py
+++ core/models.py
@@ -4,24 +4,6 @@ class BaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-class Register(BaseModel):
-    telegram_id = models.BigIntegerField(unique=True)
-    username = models.CharField(max_length=100)
-    fio = models.CharField(max_length=255, null=True)
-    pnfl = models.BigIntegerField(unique=True, null=True)
-    tg_tel = models.CharField(max_length=15, null=True)
-    tel = models.CharField(max_length=15, null=True)
-    parent_tel = models.CharField(max_length=15, null=True)
-    address = models.CharField(max_length=255, null=True)
-    is_active = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.fio
-    
-    class Meta:
-        verbose_name_plural = "Register"
-        db_table = 'register'
-
 class TelegramGroup(models.Model):
     group_name = models.CharField(max_length=100)
     group_id = models.BigIntegerField(unique=True, db_index=True)
@@ -35,3 +17,22 @@ class TelegramGroup(models.Model):
         db_table = 'telegram_group'
         verbose_name = 'Telegram Group'
         verbose_name_plural = 'Telegram Groups'
+
+class Register(BaseModel):
+    telegram_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=100)
+    fio = models.CharField(max_length=255, null=True)
+    register_group_id = models.BigIntegerField(unique=True, db_index=True)
+    pnfl = models.BigIntegerField(unique=True, null=True)
+    tg_tel = models.CharField(max_length=15, null=True)
+    tel = models.CharField(max_length=15, null=True)
+    parent_tel = models.CharField(max_length=15, null=True)
+    address = models.CharField(max_length=255, null=True)
+    is_active = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.fio
+    
+    class Meta:
+        verbose_name_plural = "Register"
+        db_table = 'register'
