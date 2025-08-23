a30d5690b3bcbc02ee529b06a8565f2e66cfc53f
DeWeWO <dewel000per@gmail.com>
2025-08-23 14:11:44 +0500

    masofaviy talim

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main_view
+from .views import main_view, table_register
 
 urlpatterns = [
-    path('', main_view, name="main_view")
+    path('', main_view, name="main_view"),
+    path('register/table/', table_register, name='table_register')
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def main_view(request):
     return render(request, 'core/index.html')
+
+def table_register(request):
+    return render(request, 'core/pages/tables/tasdiqlash.html')
