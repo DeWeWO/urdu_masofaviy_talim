22d1887b38dd60277c2e06ee68050cfc011bc56c
DeWeWO <dewel000per@gmail.com>
2025-09-02 12:45:17 +0500

    masofaviy talim

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from .views import main_view, table_register, bulk_update_register, send_message
 
 urlpatterns = [
     path('', main_view, name="main_view"),
-    path('register/table/', table_register, name='table_register'),
+    path('table_register/', table_register, name='table_register'),
     path('bulk_update_register/', bulk_update_register, name='bulk_update_register'),
     path('send-message/', send_message_to_group, name='send_message_to_group'),
 ]
